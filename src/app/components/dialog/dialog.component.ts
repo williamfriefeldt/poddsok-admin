6050a1aa9aac7bd79ad58a99a510faed0effb766
William Friefeldt <william.friefeldt@gmail.com>
2020-07-02 10:31:53 +0200

    Comments on dialog comp.

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -8,20 +8,35 @@ import { DialogOption } from '../../interfaces/dialog';
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
+
+/**
+ * @description Creates a dialog modal.
+ */
+
 export class DialogComponent implements OnInit{
 
 	dialogText: string;
 
+  /**
+   * @param { MatDialogRef } - Dialog from Material library. 
+   */
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject( MAT_DIALOG_DATA ) private data: DialogOption
   ) {}
 
+  /**
+   * @description Set intial values depending on where the user clicked.
+   */
   ngOnInit(): void {
   	const type = DIALOGTTEXT.find( obj => obj.type === this.data.type );
   	this.dialogText = type.text;
   }
 
+  /**
+  * @description Close the dialog and set value for what's next. 
+  * @param { boolean } val - user accepted task or not.
+  */
   close( val ): void {
     this.data['val'] = val;
     this.dialogRef.close( this.data );
